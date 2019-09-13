ae2aac5e5b720103f3e06c4289c955bfeb9733a1
Arthur Milchior <Arthur@Milchior.fr>
2019-09-14 01:17:17 +0200

    More

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@ from aqt import mw
 from aqt.utils import showWarning, tooltip
 from anki.hooks import addHook
 from anki.notes import Note
-from anki.utils import timestampID
 from anki.lang import _
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -12,8 +11,11 @@ from .consts import *
 
 def onMerge(browser):
     note = mergeNids(browser.selectedNotes())
-    browser.model.selectedCards = {card.id:True for card in note.cards()}
+    cards = note.cards()
     browser.search()
+    browser.model.selectedCards = {card.id:True for card in cards}
+    #browser.model.focusedCard = cards[0].id
+    browser.model.restoreSelection()
 
 def mergeNids(nids):
     if len(nids)!=2:
@@ -22,22 +24,28 @@ def mergeNids(nids):
     nid1, nid2 = nids
     note1 = Note(mw.col, id=nid1)
     note2 = Note(mw.col, id=nid2)
+    model = note1.model()
+    model2 = note2.model()
+    if model != model2:
+        showWarning(_("Please select notes of the same type to merge them"))
+        return
     return mergeNotes(note1, note2)
 
 def mergeNotes(note1, note2):
-    mw.checkpoint("Copy Notes")
-    mw.progress.start()
+    mw.checkpoint("Merge Notes")
 
     model = note1.model()
     model2 = note2.model()
     if model != model2:
         showWarning(_("Please select notes of the same type to merge them"))
         return
-    note = Note(mw.col, model)
+    note = Note(mw.col, id=note1.id)
+    if not getUserOption("Delete original cards", True):
+        note.id = timestampID(mw.col.db, "note", note.id)
+
     for i in range(len(note.fields)):
         field1 = note1.fields[i]
         field2 = note2.fields[i]
-        note.fields[i] = field1
         if field1 != field2 or not getUserOption("When identical keep a single field", True):
             note.fields[i] += field2
     cards = [None] * len(model['tmpls'])
@@ -52,24 +60,21 @@ def mergeNotes(note1, note2):
             cards[ord] = card
 
     # tags
-    note.addTag(note1.stringTags())
     note.addTag(note2.stringTags())
     note.addTag(f"merged merged_{note1.id} merged_{note2.id}")
 
     for card in cards:
         if card is None:
             continue
-        card.id = timestampID(mw.col.db, "cards")
+        if not getUserOption("Delete original cards", True):
+            card.id = timestampID(mw.col.db, "cards", card.id)
         card.nid = note.id
         card.flush()
     note.flush()
 
     if getUserOption("Delete original cards", False):
-        mw.col.remNotes([note1.id, note2.id])
+        mw.col.remNotes([note2.id])
 
-    mw.progress.finish()
-    mw.col.reset()
-    mw.reset()
     tooltip(_("Notes merged"))
     return note
 
@@ -79,3 +84,12 @@ def setupMenu(browser):
     a.triggered.connect(lambda : onMerge(browser))
     browser.form.menuEdit.addAction(a)
 addHook("browser.setupMenus", setupMenu)
+
+def timestampID(db, table, t=None):
+    "Return a non-conflicting timestamp for table."
+    # be careful not to create multiple objects without flushing them, or they
+    # may share an ID.
+    t = t or intTime(1000)
+    while db.scalar("select id from %s where id = ?" % table, t):
+        t += 1
+    return t
